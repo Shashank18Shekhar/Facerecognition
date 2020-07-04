d57d6b4663e0a02d4289773d086a481e65b5d18a
Shashank18Shekhar <sshashankshekhar1998@gmail.com>
2020-07-04 15:23:31 +0530

    Updates

---
+++ precache-manifest.5ac8d3f8d949766e27b3697eee656a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c049eebf27a95a11f804",
+    "url": "/FaceRecognitionBrain/static/css/main.ba9bdaf9.chunk.css"
+  },
+  {
+    "revision": "c049eebf27a95a11f804",
+    "url": "/FaceRecognitionBrain/static/js/main.6298d000.chunk.js"
+  },
+  {
+    "revision": "912e1d3cef19570286e6",
+    "url": "/FaceRecognitionBrain/static/js/runtime~main.c502b10d.js"
+  },
+  {
+    "revision": "706930dc4e23dc596697",
+    "url": "/FaceRecognitionBrain/static/css/2.f262f84e.chunk.css"
+  },
+  {
+    "revision": "706930dc4e23dc596697",
+    "url": "/FaceRecognitionBrain/static/js/2.0113bde6.chunk.js"
+  },
+  {
+    "revision": "2937b179f92e02d2c1afb9734f0c2de8",
+    "url": "/FaceRecognitionBrain/index.html"
+  }
+];
